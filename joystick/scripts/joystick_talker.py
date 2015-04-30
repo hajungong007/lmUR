27aecc5c473ab8722dd273ab5e9f1e0142925849
GSala <gsala24@gmail.com>
2015-04-30 13:12:06 +0200

    Changed driver for the new joystick (Logitech Extreme3D Pro)

--- joystick/scripts/joystick_talker.py
+++ joystick/scripts/joystick_talker.py
@@ -39,55 +39,42 @@ def buttonAction(action):
     if action[4] == 1:
         # Button pressed
         if buttonNumber == 1:
-            # print 'Stuff'
+            # Trigger
             msg.hand_available = True
         elif buttonNumber == 2:
-            msg.palm_position.z = 127
-        elif buttonNumber == 3:
-            msg.palm_position.z = -128
-        elif buttonNumber == 4:
-            msg.palm_position.x = -128
-        elif buttonNumber == 5:
-            msg.palm_position.x = 127
-        elif buttonNumber == 8:
-            msg.ypr.x = -45
-        elif buttonNumber == 9:
-            msg.ypr.x = 45
+            # Grab
+            msg.grab_action = True
+
     elif action[4] == 0:
         # Button released
         if buttonNumber == 1:
+            # Trigger
             msg.hand_available = False
-        elif buttonNumber == 2 or buttonNumber == 3:
-            msg.palm_position.z = 0
-        elif buttonNumber == 4 or buttonNumber == 5:
-            msg.palm_position.x = 0
-        elif buttonNumber == 8 or buttonNumber == 9:
-            msg.ypr.x = 0
+        elif buttonNumber == 2:
+            # Grab
+            msg.grab_action = False
 
 def joystickMoved(action):
     global msg
     value = converToSignedByte(action[5])
     if action[7] == 0:
-        # X axis movement
+        # Roll movement
         msg.ypr.z = round((value+0.5) / 1.41666666666)
     elif action[7] == 1:
-        # Y axis movement
+        # Pitch movement
         msg.ypr.y = round((value+0.5) / 1.41666666666)
     elif action[7] == 2:
-        # Z axis movement
+        # Yaw movement
+        msg.ypr.x = round((value+0.5) / 1.41666666666)
+    elif action[7] == 3:
+        # Y movement
         msg.palm_position.y = -(value + 1) + 165 # Invert
-
-# while 1:
-#     for byte in pipe.read(1):
-#         action += [ord(byte)]
-#         if len(action) == 8:
-#             # print action
-#             if isButtonAction(action):
-#                 buttonAction(action)
-#             elif isJoystickMovement(action):
-#                 joystickMoved(action)
-#
-#             action = []
+    elif action[7] == 4:
+        # X movement
+        msg.palm_position.x = value
+    elif action[7] == 5:
+        # Z movement
+        msg.palm_position.z = value
 
 def talker():
     global msg
@@ -99,7 +86,6 @@ def talker():
         for byte in pipe.read(1):
             action += [ord(byte)]
             if len(action) == 8:
-                # print action
                 if isButtonAction(action):
                     buttonAction(action)
                 elif isJoystickMovement(action):
